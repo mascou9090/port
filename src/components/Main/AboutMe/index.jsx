50d4b411850dc67e4d849e089c97a793e02afea3
Marcondes F Pinheiro <bshoopkss@gmail.com>
2022-07-15 17:25:42 -0300

    firstCommit

---
+++ src/components/Main/AboutMe/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { About } from './style'
+
+export const AboutMe = () => {
+  return (
+    <About>
+      <h4>Desenvolvedor Front End</h4>
+      
+    </About>
+  )
+}
